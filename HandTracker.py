8d10b4ed88d776e984faa21a690b5871e3125759
uaikhuel <89050866+uaikhuel@users.noreply.github.com>
2023-04-24 18:27:08 -0400

    Pillow Download for Images and Countdown Function Timer Setup
    In my latest update, I have installed Pillow onto my computer via Terminal in order for my HandTracker to print my stretch demonstration images when called in my app. I have also created three functions: countdown60, countdown30, and countdown 15 to use as timers for the length of each set/rep of stretches, as well as the length of each break.

--- HandTracker.py
+++ HandTracker.py
@@ -1,9 +1,45 @@
 import time
+from PIL import Image
 
+countdownsec = 0
 
 print("Please schedule a good time for us to begin our stretch session:")
 time_hourmin = input("When would you like to schedule our next session?:")
 time_hourmin = time(time_hourmin)
+
+def countdown60(countdownsec):
+
+
+    seconds = 60
+
+    for i in range(seconds):
+        print(str(seconds-i) + " seconds remaining \n")
+
+    time.sleep(1)
+    print("Time is up")
+
+def countdown30(countdownsec):
+
+
+    seconds = 30
+
+    for i in range(seconds):
+        print(str(seconds-i) + " seconds remaining \n")
+
+    time.sleep(1)
+    print("Time is up")
+
+def countdown15(countdownsec):
+
+
+    seconds = 15
+
+    for i in range(seconds):
+        print(str(seconds-i) + " seconds remaining \n")
+
+    time.sleep(1)
+    print("Time is up")
+
 
 
 def warm_up():
@@ -11,8 +47,9 @@ def warm_up():
 
     if warmup_repeat <= 4:
         print("Please perform a warm-up before we begin:")
-        image: ctwarmup.webp
-        countdown(30sec)
+        im = Image.open("ctwarmup.webp")
+        im.show()
+        countdown15(countdownsec)
     else:
         print("Please schedule a good time for us to begin our stretch session:")
         time_hourmin = input("When would you like to schedule our next session?:")
@@ -25,15 +62,17 @@ def scheduled_stretch():
 
     if  flex_repeat <= 4:
      print("Please complete a Wrist flexor stretch:")
-     image: ctwristflexorstretch.jpeg
-     countdown(30 sec)
-     rest(15 sec)
+     im2 = Image.open("ctwristflexorstretch.jpeg")
+     im2.show()
+     countdown60(countdownsec)
+     countdown15(countdownsec)
      flex_repeat += 1
     if extend_repeat <= 4:
      print("Please complete a Wrist extensor stretch:")
-     image: ctwristextensorstretch.jpeg
-     countdown(30 sec) 
-     rest(15 sec)
+     im3 = Image.open("ctwristextensorstretch.jpeg")
+     im3.show()
+     countdown60(countdownsec)
+     countdown15(countdownsec)
      extend_repeat += 4
 
 
